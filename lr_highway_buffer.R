64a4ac7774e65887a4795ac2c312b79b6e88370c
Thanicha Ruangmas <ruangmas@umd.edu>
2024-06-04 13:28:52 -0400

    edit code with description

--- lr_highway_buffer.R
+++ lr_highway_buffer.R
@@ -21,11 +21,12 @@ lr_project<-project(char_lr, "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs "
 
 #make 10 km buffer around light rail centroid
 lrc<-centroids(lr_project, inside=FALSE)
-lrc_buff<-buffer(lrc, width = 10000)
-writeVector(lrc_buff, "Houston Shapefiles/lr_1km_buff", overwrite=TRUE)
+#lrc_buff<-buffer(lrc, width = 10000)
+#writeVector(lrc_buff, "Houston Shapefiles/lr_1km_buff", overwrite=TRUE)
+lrc_buffer<-vect("Houston Shapefiles/lr_1km_buff")
 
 #make 1 km buffer around light rail route 
-lr_buffer<-terra::buffer(lr_project, width = 1000)
+#lr_buffer<-terra::buffer(lr_project, width = 1000)
 
 #get roads data
 r<-vect("G:/Shared drives/2024 FIRE Light Rail/DATA/tl_2021_txharris_roads/tl_2021_48201_roads.shp")
@@ -37,24 +38,24 @@ o<-subset(r_project, r_project$RTTYP=="O")
 s<-subset(r_project, r_project$RTTYP=="S")
 
 #make plot of houston and light rail buffer
-bg <- get_tiles(ext(lrc_buff)) #choose a number that will give a good picture of the light rail area
+bg <- get_tiles(ext(lr_project)) #choose a number that will give a good picture of the light rail area
 plot(bg)
 lines(lr_project, col="blue")
-lines(lr_buffer, col="black")
-lines(lrc_buff, col="darkgreen")
+#lines(lr_buffer, col="black")
+lines(lrc_buffer, col="darkgreen")
 lines(s, col="purple")
 lines(i, col="red")
 
 #crop s and i highways that are within 10 km of the light rail centroid
-sint<-crop(s, lrc_buff)
-iint<-crop(i, lrc_buff)
+sint<-crop(s, lrc_buffer)
+iint<-crop(i, lrc_buffer)
 u<-aggregate(rbind(sint, iint), dissolve=TRUE)
 
 #make a buffer around major roads but take out areas that overlap with the light rail
 u_buffer<-terra::buffer(u, width = 1000)
 u_nolr<-erase(u_buffer, lr_buffer)
   
-bg <- get_tiles(ext(lrc_buff)) #choose a number that will give a good picture of the light rail area
+bg <- get_tiles(ext(lrc_buffer)) #choose a number that will give a good picture of the light rail area
 plot(bg)
 lines(lr_project, col="blue")
 lines(lr_buffer, col="black")
