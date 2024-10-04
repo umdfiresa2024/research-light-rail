1048b2e9637fe4ad53b12f472c8a8cb8d3285a09
Thanicha Ruangmas <106334045+ruangmas@users.noreply.github.com>
2024-10-04 10:41:23 -0400

    add sample did regressions

--- sample_did_charlotte.R
+++ sample_did_charlotte.R
@@ -1,114 +1,3 @@
-library("tidyverse")
-
-
-####pm2.5 houston and phoenix#######################################
-
-f<-dir("G:/Shared drives/2024 FIRE Light Rail/DATA/Houston & Phoenix")
-
-p_indv<-c()
-for (i in 1:length(f)) {
-  print(f[i])
-  pdf<-read.csv(paste0("G:/Shared drives/2024 FIRE Light Rail/DATA/Houston & Phoenix/",f[i])) |>
-    mutate(year=substr(date, 1, 4)) |>
-    mutate(month=substr(date, 5, 6)) |>
-    mutate(day=substr(date, 7,8))
-  
-  p_indv<-rbind(p_indv, pdf)
-}
-
-p_tx_az<-p_indv
-
-p_tx_az2<-p_tx_az |>
-  mutate(Address=ifelse(city_num==1, "Phoenix-Mesa, AZ", "Houston, TX")) |>
-  select(-city_num)
-
-###charlotte#######################################
-
-f<-dir("Charlotte_TA_PM25/")
-
-p_indv<-c()
-for (i in 1:length(f)) {
-  print(f[i])
-  pdf<-read.csv(paste0("Charlotte_TA_PM25/",f[i])) |>
-    mutate(year=substr(date, 1, 4)) |>
-    mutate(month=substr(date, 5, 6)) |>
-    mutate(day=substr(date, 7,8))
-  
-  p_indv<-rbind(p_indv, pdf)
-}
-p_nc<-p_indv
-
-p_nc2<-p_nc |>
-  mutate(Address="Charlotte, NC")
-
-###mn######################################
-
-f<-dir("Twin_Cities_TA_PM25/")
-
-p_indv<-c()
-for (i in 1:length(f)) {
-  print(f[i])
-  pdf<-read.csv(paste0("Twin_Cities_TA_PM25/",f[i])) |>
-    mutate(year=substr(date, 1, 4)) |>
-    mutate(month=substr(date, 5, 6)) |>
-    mutate(day=substr(date, 7,8))
-  
-  p_indv<-rbind(p_indv, pdf)
-}
-p_mn<-p_indv
-
-p_mn2<-p_mn |>
-  mutate(Address="Minneapolis-St. Paul, MN")
-
-p_trt<-rbind(p_mn2, p_nc2, p_tx_az2)
-
-write.csv(p_trt, "daily roads/pm25_daily_roads.csv", row.names=F)
-
-####PM2.5 around control highways##############################################
-cgroup<-read.csv("cntrl_road_cities.csv") |>
-  rename(city_num=X) |>
-  select(city_num, Address)
-
-f<-dir("PM25_daily", pattern="cntrl_highways")
-
-p_indv<-c()
-for (i in 1:length(f)) {
-  print(f[i])
-  pdf<-read.csv(paste0("PM25_daily/",f[i])) |>
-    mutate(year=substr(date, 1, 4)) |>
-    mutate(month=substr(date, 5, 6)) |>
-    mutate(day=substr(date, 7,8))
-  
-  p_indv<-rbind(p_indv, pdf)
-}
-
-p_cntrl<-p_indv
-
-p_cntrl2<-merge(p_cntrl, cgroup, by="city_num")
-
-write.csv(p_cntrl2, "daily roads/pm25_daily_roads_cntrl.csv", row.names=F)
-
-######met#########################################################
-
-f<-dir("G:/Shared drives/2024 FIRE Light Rail/DATA/cntrl_met_data")
-
-p_indv<-c()
-for (i in 1:length(f)) {
-  print(f[i])
-  pdf<-read.csv(paste0("G:/Shared drives/2024 FIRE Light Rail/DATA/cntrl_met_data/",f[i])) |>
-    mutate(year=substr(date, 1, 4)) |>
-    mutate(month=substr(date, 5, 6)) |>
-    mutate(day=substr(date, 7,8))
-  
-  p_indv<-rbind(p_indv, pdf)
-}
-p_met<-p_indv |>
-  rename(city_num=ID)
-
-p_met2<-merge(p_met, cgroup, by="city_num")
-
-write.csv(p_met2, "daily roads/met_daily_cntrls.csv", row.names=F)
-
 ##########combine everything#####################################################
 library("tidyverse")
 
@@ -139,9 +28,9 @@ rm(trt_met, trt_pm, cntrl_pm, cntrl_met)
 
 df<-bind_rows(trt,cntrl) |>
   select(-city_num) |>
-  mutate(start=as.Date("01/12/2003", format="%d/%m/%Y")) |>
+  mutate(start=as.Date("01/12/2003", format="%d/%m/%Y")) |> #start date of dataset
   mutate(open=as.Date("01/11/2007", format="%d/%m/%Y")) |>
-  mutate(end=as.Date("01/11/2011", format="%d/%m/%Y")) |>
+  mutate(end=as.Date("01/11/2011", format="%d/%m/%Y")) |> #end date of dataset
   mutate(opentime=ifelse(as.Date(date)>open,1,0)) |>
   mutate(treatcity=ifelse(Address=="Charlotte, NC",1,0)) |>
   mutate(time=interval(start, date)/days(1)) |>
@@ -153,7 +42,7 @@ df<-bind_rows(trt,cntrl) |>
 
 library("Synth")
 library("gsynth")
-  
+
 
 dataprep.out <-
   dataprep(
@@ -198,7 +87,7 @@ gaps.plot(synth.res    = synth.out,
 
 abline(v   = 1432,
        lty = 2)
-  
+
 synth_control = dataprep.out$Y0plot %*% synth.out$solution.w
 #####################################################
 
@@ -221,6 +110,7 @@ summary(m1<-felm(log(pm25) ~ opentime:trtcity + opentime + trtcity + TWS_tavg + 
                  | dow + month + year, data=df2))
 
 
+###met files for other cities###################################################
 
 met2<-read.csv("daily roads/met_houston.csv") |>
   mutate(date=as.Date(substr(date, 1,8), "%Y%m%d")) |>
@@ -234,4 +124,3 @@ met4<-read.csv("daily roads/met_twin_cities.csv") |>
   mutate(date=as.Date(substr(date, 1,8), "%Y%m%d")) |>
   mutate(Address="Minneapolis-St. Paul, MN") |>
   select(-X)
-  
